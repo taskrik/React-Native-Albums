303d29b799abb811440fe66fcbaef5cbacd9c513
Tasos <tasos.krikonis@gmail.com>
2018-09-29 16:42:49 +0200

    cardSection

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 import Card from './Card'
+import CardSection from './CardSection';
+
+
+
 export default class AlbumDetails extends Component {
   render() {
     return (
       <Card>
-        <Text> {this.props.album.title} </Text>
+        <CardSection>
+            <View></View>
+            <View>
+                <Text>{this.props.album.title}</Text>
+                <Text>{this.props.album.artist}</Text>
+            </View>
+        </CardSection>
       </Card>
     )
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    headerContentStyle:{
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
+})
